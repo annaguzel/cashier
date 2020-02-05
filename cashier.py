5b803525f9f3a5b4d0c986519cd251be69009f5f
Annaguzel <you@example.com>
2020-02-05 15:55:35 +0200

    Updated file for cashier task

--- cashier.py
+++ cashier.py
@@ -1,5 +1,28 @@
 def main():
+	list_items=[]
+	stop = ''
+	total=0
+	while stop != 'done':
+		item=input('Item (enter "done" when finished):')
+		if (item != 'done'):
+			price=int(input('Price:'))
+			quantity=int(input('Quantity:'))
+			list_items.append({'name':item,'price':price,'quantity':quantity})
+		else:
+			stop=item
+
+
+
 	# write code here
+	print('---------------\n receipt\n--------------')
+	for item in list_items:
+		total_item=item['price']*item['quantity']
+		print (item['quantity'],item['name'],item['price']*item['quantity'],'KD')
+		total+=total_item
+
+	print('Total Price: ',total,'KD')
+
+
 
 if __name__ == '__main__':
 	main()
